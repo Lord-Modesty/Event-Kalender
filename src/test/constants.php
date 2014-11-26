5e087f0a85573eb434cf455c6e14f0df676dd02c
Nicola Bischof <n.bischof@outlook.com>
2014-11-26 17:51:55 +0100

    Revert "Revert ":globe_with_meridians:""
    This reverts commit 80282483c9cd93de7bf34b58155ca51a485aa587.

---
+++ src/test/constants.php
@@ -0,0 +1,6 @@
+ <?php 
+ defined('MYSQL_HOST') ? null : define("MYSQL_HOST", "localhost"); 
+ defined('MYSQL_USER') ? null : define("MYSQL_USER", "root"); 
+ defined('MYSQL_PASS') ? null : define("MYSQL_PASS", ""); 
+ defined('MYSQL_DBNAME') ? null : define("MYSQL_DBNAME", "eventkalender"); 
+?> 
