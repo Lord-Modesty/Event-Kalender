5f437a46d79412b68bb32e9380fbbda2c8d96836
Nicola Bischof <n.bischof@outlook.com>
2014-12-12 14:20:11 +0100

    :gem:  Added slide-in menue

--- src/test/index/index.php
+++ src/test/index/index.php
@@ -1,5 +1,4 @@
-<?php session_start(); 
-$_SESSION[ 'CurrentSite']="Events" ; ?>
+<?php session_start(); $_SESSION[ 'CurrentSite']="Events" ; ?>
 <!doctype html>
 <html>
 
@@ -7,18 +6,27 @@ $_SESSION[ 'CurrentSite']="Events" ; ?>
     <meta charset="UTF-8">
     <title>Event Kalender | Index</title>
     <link rel="stylesheet" href="../css/bootstrap.css">
+    <link href="../css/reset.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
     <link rel="shortcut icon" href="../img/calendar.png" type="image/x-icon" />
-
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,600,500,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700,900,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 
 <body>
 
     <?php include( "../navbar_footer/navbar.php")?>
+    <h1>EVENTKALENDER</h1>
+
+   
 
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    </div>
+    </div>
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="../js/custom.js"></script>
     <script src="../js/bootstrap.min.js"></script>
 </body>
 
--- src/test/navbar_footer/navbar.php
+++ src/test/navbar_footer/navbar.php
@@ -1,40 +1,42 @@
-<div class="navbar navbar-inverse">
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-inverse-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand">
-            <?php echo $_SESSION[ 'CurrentSite'] ?>
-        </a>
-    </div>
-    <div class="navbar-collapse collapse navbar-inverse-collapse">
+<div id="container">
 
-        <ul class="nav navbar-nav">
-            <li><a href="../index/index.php" data-toggle="tooltip" data-placement="bottom" title="Events">
-                <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>
-                </a>
-            </li>
+    <div id="canvas">
 
-            <li>
-                <a href="#" data-toggle="tooltip" data-placement="bottom" title="Archiv">
-                    <span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>
-                </a>
-            </li>
-        </ul>
-        <?php if ($_SESSION[ 'CurrentSite']=="Login" ) { } else{ echo '<ul class="nav navbar-nav navbar-right">
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    <span class="glyphicon glyphicon-user" aria-hidden="true">  </span>
-                    <b class="caret"></b>
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a href="../login/login.php"><span class="glyphicon glyphicon-lock" aria-hidden="true"> Login</span>
-                       </a>
-                    </li>
-                </ul>
-            </li>
-        </ul>'; } ?>
-    </div>
-</div>
+        <div id="nav">
+            <h2 id="title"></i> MENU</h2>
+            <ul id="toggle">
+                <li>
+                    <div class="active border">
+                        <span class="menu-icons fa fa-home"></span>  <a href="#">HOME</a>
+                    </div>
+                </li>
+
+                <li>
+                    <div>
+                        <span class="menu-icons  fa fa-database"></span>  <a href="#">ARCHIV</a>
+                    </div>
+                </li>
+
+                <li>
+                    <div>
+                        <span class="menu-icons  fa fa-user"></span>
+                        <a href="#">ADMIN</a><span class="the-btn fa fa-plus"></span>
+                    </div>
+
+                    <ul>
+                        <li>
+                            <a href="#">LOGIN</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li>
+                    <div>
+                        <span class="menu-icons  fa fa-info"></span>
+                        <a href="#">ABOUT</a> 
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <a href="#" class="toggle-nav" id="bars"><i class="fa fa-bars"></i></a>
+   
