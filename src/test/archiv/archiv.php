fb84742271d68b22775d279187ca8e700aea2158
Nicola Bischof <n.bischof@outlook.com>
2014-12-14 14:08:57 +0100

    :globe_with_meridians:  Added archiv

---
+++ src/test/archiv/archiv.php
@@ -0,0 +1,26 @@
+<?php session_start(); 
+    $_SESSION[ 'CurrentSite']="Archiv"; 
+?>
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Event Kalender | Archiv</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="shortcut icon" href="../img/calendar.png" type="image/x-icon" />
+
+</head>
+
+<body>
+
+    <?php include( "../navbar_footer/navbar.php")?>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../js/bootstrap.min.js"></script>
+</body>
+
+</html>
